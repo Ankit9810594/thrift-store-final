b87abac014ff7b394242111a5e59096a42b8f70f
samyog27 <57705758+samyog27@users.noreply.github.com>
2022-07-18 00:25:21 -0700

    thrift-store-final

---
+++ src/services/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAV_KfnY3Tk0-CY-xkVjwI1n6WzM5XZul0",
+  authDomain: "thrift-store-1ce13.firebaseapp.com",
+  databaseURL: "https://thrift-store-1ce13-default-rtdb.firebaseio.com",
+  projectId: "thrift-store-1ce13",
+  storageBucket: "gs://thrift-store-1ce13.appspot.com",
+  messagingSenderId: "40807701682",
+  appId: "1:40807701682:web:da6004f5d13ec934327dab",
+  measurementId: "G-HCZ10LMSQ7"
+};
+
+export default firebaseConfig;
---
+++ src/views/home/index.jsx
@@ -0,0 +1,93 @@
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { MessageDisplay } from 'components/common';
+import { ProductShowcaseGrid } from 'components/product';
+import { FEATURED_PRODUCTS, RECOMMENDED_PRODUCTS, SHOP } from 'constants/routes';
+import {
+  useDocumentTitle, useFeaturedProducts, useRecommendedProducts, useScrollTop
+} from 'hooks';
+import bannerImg from 'images/banner-girl.png';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Home = () => {
+  useDocumentTitle('Lunetas-cam | Home');
+  useScrollTop();
+
+  const {
+    featuredProducts,
+    fetchFeaturedProducts,
+    isLoading: isLoadingFeatured,
+    error: errorFeatured
+  } = useFeaturedProducts(6);
+  const {
+    recommendedProducts,
+    fetchRecommendedProducts,
+    isLoading: isLoadingRecommended,
+    error: errorRecommended
+  } = useRecommendedProducts(6);
+
+  return (
+    <main className="content">
+      <div className="home">
+        <div className="banner">
+          <div className="banner-desc">
+            <h1 className="text-thin">
+              <strong>Explore</strong>
+              &nbsp;our huge range of
+quality&nbsp;
+              <strong>second-hand fashion</strong>
+            </h1>
+            <p>
+            We have over 250,000 quality-assured second-hand items in stock, with fast delivery and 30 day returns.
+            </p>
+            <br />
+            <Link to={SHOP} className="button">
+              Shop Now &nbsp;
+              <ArrowRightOutlined />
+            </Link>
+          </div>
+          <div className="banner-img"><img src={bannerImg} alt="" /></div>
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Featured Products</h1>
+            <Link to={FEATURED_PRODUCTS}>See All</Link>
+          </div>
+          {(errorFeatured && !isLoadingFeatured) ? (
+            <MessageDisplay
+              message={errorFeatured}
+              action={fetchFeaturedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={featuredProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Recommended Products</h1>
+            <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
+          </div>
+          {(errorRecommended && !isLoadingRecommended) ? (
+            <MessageDisplay
+              message={errorRecommended}
+              action={fetchRecommendedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={recommendedProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
